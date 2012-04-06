9152ad3828538b7090fde0e1522e6d5e49e671c7
eric <k09089@buzzni.com>
2012-04-06 20:50:42 +0900

    mailer

---
+++ pre.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from GmailConnector import GmailConnector
+from GmailConnector import get_members
+from datetime import date
+
+if __name__ == "__main__":
+    members = [ a["email"] for a in get_members() ]
+
+    content = """
+    * 버즈니 점심 사전조사 * // %s
+    안드실분은 시간내에 답장해주세요.
+    """
+
+    content = content % (date.today())
+
+    gc = GmailConnector()
+
+    gc.send(["pgonee@buzzni.com"], "pre", content)
+#    gc.send(members, "pre", content)
